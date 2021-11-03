99ec4c2ffd1b9885d269c5502a9985dbb450919a
vipin0 <vipin6673@gmail.com>
2021-11-03 17:33:21 +0530

    Added ICAO resolver and error handeling
    - Added ICAO reolver using utility
    - Changer print function to print name and detail of airport.

--- flight-tracker.py
+++ flight-tracker.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 from tabulate import tabulate
 
 from utils.date import DateTimeConversion
+from utils.icao_code import get_airport_detail
 
 class FlightTracker(DateTimeConversion):
     """This class provide all the functionalities for the FlightTracker.
@@ -19,6 +20,8 @@ class FlightTracker(DateTimeConversion):
         self.BASE_URL = "https://opensky-network.org/api"
 
     def __get_flights(self,query_type,airport,begin:datetime,end:datetime):
+        """This method gets all the flights for the given query and time interval.
+        """
         begin = self.datetime_to_unix(begin)
         end = self.datetime_to_unix(end)
         url = f"{self.BASE_URL}/flights/{query_type}?airport={airport}&begin={begin}&end={end}"
@@ -30,10 +33,13 @@ class FlightTracker(DateTimeConversion):
         time_var = "lastSeen"
         if query_type == "departure":
             time_var = "firstSeen"
-        flightdetails = [[f["icao24"],f["estDepartureAirport"],f["estArrivalAirport"],self.unix_to_datetime(f[time_var])] for f in result]
+        flightdetails = [[f["icao24"],f["estDepartureAirport"],f["estArrivalAirport"],self.unix_to_datetime(f[time_var])]
+                             for f in result if f["estDepartureAirport"] and f["estArrivalAirport"]]
         return flightdetails
 
     def get_arrivals(self,airport,begin=datetime.now()-timedelta(7),end=datetime.now()):
+        """This method gets all the arriving flights for the given query and time interval.
+        """
         flightdetails = [["Flight No.","Departed From","Coming to","ETA"]]
 
         flightdetails += self.__get_flights("arrival",airport=airport,begin=begin,end=end)
@@ -41,19 +47,24 @@ class FlightTracker(DateTimeConversion):
         return flightdetails
 
     def get_departures(self,airport,begin=datetime.now()-timedelta(7),end=datetime.now()):
+        """This method gets all the departing flights for the given query and time interval.
+        """
         flightdetails = [["Flight No.","Departed From","Going To","ETD"]]
         flightdetails += self.__get_flights("departure",airport=airport,begin=begin,end=end)
         return flightdetails
 
-    def print_flights(self,flightdetails):
+    def print_flights(self,flightdetails,ap_detail):
+        """This method gets print all the flights for the given query and time interval.
+        """
         print("*"*65)
-        print("Flight Details".center(65),end="\n")
+        print("Flight Details".center(65),end="\n\n")
+        print(ap_detail.center(65),end="\n")
         print("*"*65)
-        print("\n\n",tabulate(flightdetails,headers="firstrow"))
+        print("\n",tabulate(flightdetails,headers="firstrow"))
 
 
 @click.command()
-@click.argument("ICAOCODE")
+@click.argument("airport_name_or_city_name")
 @click.option('-a', '--arrival',is_flag=True,help="list the arriving airplanes to the given airport.")
 @click.option('-d', '--depart',is_flag=True,help="list the depaturting airplanes from the given airport.")
 @click.option('-b', '--begin',type=click.DateTime(formats=["%Y-%m-%d %H:%M:%S"])
@@ -61,8 +72,22 @@ class FlightTracker(DateTimeConversion):
 @click.option('-e', '--end',type=click.DateTime(formats=["%Y-%m-%d %H:%M:%S"]),
             help="ending time in Y-m-d H:M:S")
 
-def main(icaocode,arrival,depart,begin,end):
+def main(airport_name_or_city_name,arrival,depart,begin,end):
+    """main method of the script."""
     # print(f"{arrival}\n{depart}\n{begin}\n{end}")
+
+    details = get_airport_detail(airport_name_or_city_name)
+    if len(details) == 0:
+        print("No airport code found with given name. Try a finer search!!",file=sys.stderr)
+        sys.exit(1)
+    elif len(details) != 1:
+        print("Multiple airport codes found with given name. Try a finer search!!",file=sys.stderr)
+        sys.exit(1)
+    else:
+        icaocode = details[0]["icao"]
+        name = details[0]["name"]
+        city = details[0]["city"] 
+        ap_detail = f"{name}, {city} - ICAO : {icaocode}"
 
     f = FlightTracker()
     if arrival:
@@ -80,7 +105,7 @@ def main(icaocode,arrival,depart,begin,end):
             result = f.get_arrivals(icaocode,begin=begin,end=end)
         else:
             result = f.get_arrivals(icaocode)
-        f.print_flights(result)
+        f.print_flights(result,ap_detail)
     
     elif depart:
         result = None
@@ -97,7 +122,7 @@ def main(icaocode,arrival,depart,begin,end):
             result = f.get_departures(icaocode,begin=begin,end=end)
         else:
             result = f.get_departures(icaocode)
-        f.print_flights(result)
+        f.print_flights(result,ap_detail)
 
 if __name__ == "__main__":
     main()
