5eb0f90d51353b82ce131f81f72be1cc7ff55ed8
vipin0 <vipin6673@gmail.com>
2021-11-05 21:35:56 +0530

    added get_airport_detail method
    - renamed get_aiport_detail -> resolve_airport_detail
    - moved get_airport_details from flight-tracker to this file and renamed
      as get_airport_detail

--- utils/icao_code.py
+++ utils/icao_code.py
@@ -2,10 +2,12 @@
 
 """
 import json
+import sys
+
 FILE_NAME = "utils/airports.json"
 
 
-def get_airport_detail(query:str):
+def resolve_airport_detail(query:str):
     """This method accept the query and return the icao code and airport detail.
     """
     # print(query)
@@ -16,4 +18,21 @@ def get_airport_detail(query:str):
         if query.lower() in data["name"].lower() or query.lower() in data["city"].lower():
             result.append(data)
     return result
+
+
+def get_airport_detail(airport_name_or_city_name):
+    """utility function to resolve airport detail"""
+    details = resolve_airport_detail(airport_name_or_city_name)
+    if len(details) == 0:
+        print("No airport code found with given name. Try a finer search!!",file=sys.stderr)
+        sys.exit(1)
+    elif len(details) != 1:
+        print("Multiple airport codes found with given name. Try a finer search!!",file=sys.stderr)
+        sys.exit(1)
+    else:
+        icaocode = details[0]["icao"]
+        name = details[0]["name"]
+        city = details[0]["city"] 
+        ap_detail = f"{name}, {city} - ICAO : {icaocode}"
+    return (icaocode,ap_detail)
 
